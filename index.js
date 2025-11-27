deb086d4f5a0666c6aeb29898f68aaaf49ff90ce
Gantabya Kumar Bayda <gantabyakprottoy96@gmail.com>
2025-11-28 00:57:55 +0600

    Finally solved the deployment issue. Now will deploy client side

--- index.js
+++ index.js
@@ -222,7 +222,7 @@ async function run() {
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
 
-        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
@@ -230,6 +230,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-// app.listen(port, () => {
-//     console.log(`product management server started on port: ${port}`);
-// })
+app.listen(port, () => {
+    console.log(`product management server started on port: ${port}`);
+})
